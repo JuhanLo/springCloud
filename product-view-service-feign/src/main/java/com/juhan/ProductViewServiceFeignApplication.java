11577a768c4907e75a9eef564c0f830ad4aae1f6
juhan <531487379@qq.com>
2019-08-01 10:09:44 +0800

    SpringCloud

--- product-view-service-feign/src/main/java/com/juhan/ProductViewServiceFeignApplication.java
+++ product-view-service-feign/src/main/java/com/juhan/ProductViewServiceFeignApplication.java
@@ -1,6 +1,6 @@
 package com.juhan;
 
-import brave.sampler.Sampler;
+
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.NetUtil;
@@ -10,7 +10,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
 import java.util.concurrent.*;
@@ -27,6 +26,13 @@ import java.util.concurrent.*;
 public class ProductViewServiceFeignApplication {
 
     public static void main(String[] args) {
+        //判断 rabiitMQ 是否启动
+        int rabbitMQPort = 5672;
+        if (NetUtil.isUsableLocalPort(rabbitMQPort)) {
+            System.err.printf("未在端口%d 发现 rabbitMQ服务，请检查rabbitMQ 是否启动", rabbitMQPort);
+            System.exit(1);
+        }
+
         int port = 0;
         final int defaultPort = 8012;
         Future<Integer> future = ThreadUtil.execAsync(new Callable<Integer>() {
@@ -62,8 +68,8 @@ public class ProductViewServiceFeignApplication {
 
     }
 
-    @Bean
-    public Sampler defaultSampler() {
-        return Sampler.ALWAYS_SAMPLE;
-    }
+//    @Bean
+//    public Sampler defaultSampler() {
+//        return Sampler.ALWAYS_SAMPLE;
+//    }
 }
--- product-view-service-feign/src/main/java/com/juhan/controller/ProductController.java
+++ product-view-service-feign/src/main/java/com/juhan/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.juhan.controller;
 import com.juhan.domain.Product;
 import com.juhan.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
+    @Value("${version}")
+    private String version;
+
     @RequestMapping("/products")
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
         m.addAttribute("ps", ps);
+        m.addAttribute("version", version);
         return "products";
     }
 }
